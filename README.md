1d9929374f71e7ee09add5e860cc6702e2e37ede
Carlos Santana <contato.albertosantana@gmail.com>
2023-04-18 17:23:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Instituto Federal de Sergipe
+#### Campus Lagarto, Bacharelado em Sistema de Informação
+
+> Disciplina: Análise e Estrutura de dados I <br>
+
+Esse repositório contém:
+* Projeto de calculadora (revisão para POO)
+* Algoritmos de ordenação
---
+++ src/revisaoPoo/Main.java
@@ -0,0 +1,54 @@
+package revisaoPoo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int operacao;
+        double number; double number2; double resultado;
+
+        do {
+
+            System.out.println("""
+                    Calculadora de terminal - Flag -1\s
+                    1. Soma           | 2. Subtracao\s
+                    3. Multiplicacao  | 4. Divisao\s
+                    Digite a opcao:
+                    """);
+
+            operacao = input.nextInt();
+
+            if (operacao != -1) {
+
+                System.out.println("Digite o primeiro numero: ");
+                number = input.nextDouble();
+                System.out.println("Digite o segundo numero: ");
+                number2 = input.nextDouble();
+
+                switch (operacao) {
+                    case 1: {
+                        resultado = number + number2;
+                        System.out.println(resultado);
+                        break;
+                    } case 2: {
+                        resultado = number - number2;
+                        System.out.println(resultado);
+                        break;
+                    } case 3: {
+                        resultado = number * number2;
+                        System.out.println(resultado);
+                        break;
+                    } case 4: {
+                        resultado = number / number2;
+                        System.out.println(resultado);
+                        break;
+                    }
+                }
+            }
+
+        } while (operacao != -1);
+    }
+}

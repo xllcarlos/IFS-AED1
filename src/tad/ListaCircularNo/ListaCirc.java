fa2f031a5e9a5ec70960901fffc698210e4e7d5a
Carlos Santana <contato.albertosantana@gmail.com>
2023-06-06 20:07:10 -0300

    updates

--- src/tad/ListaCircularNo/ListaCirc.java
+++ src/tad/ListaCircularNo/ListaCirc.java
@@ -1,4 +1,4 @@
-package tad.ListaCircular;
+package tad.ListaCircularNo;
 
 public class ListaCirc<T> {
 
--- src/tad/ListaCircularNo/NoCirc.java
+++ src/tad/ListaCircularNo/NoCirc.java
@@ -1,4 +1,4 @@
-package tad.ListaCircular;
+package tad.ListaCircularNo;
 
 public class NoCirc<T> {
 

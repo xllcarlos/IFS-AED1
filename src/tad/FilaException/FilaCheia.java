f2008fd1304616541f970256637b3be9a73b7531
Carlos Santana <contato.albertosantana@gmail.com>
2023-06-19 19:42:48 -0300

    updates

---
+++ src/tad/FilaException/FilaCheia.java
@@ -0,0 +1,10 @@
+package tad.FilaException;
+
+import tad.FilaSequencial.FilaSequencial;
+
+public class FilaCheia extends Exception {
+
+    public FilaCheia(String message) {
+        super(message);
+    }
+}
---
+++ src/tad/FilaSequencial/Main.java
@@ -0,0 +1,26 @@
+package tad.FilaSequencial;
+
+import tad.FilaException.FilaCheia;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FilaSequencial filaSequencial = new FilaSequencial(18);
+
+        System.out.println(filaSequencial.filaCheia());
+        System.out.println(filaSequencial.filaVazia());
+
+        try {
+            filaSequencial.adicionar(2);
+            filaSequencial.adicionar(5);
+            filaSequencial.adicionar(8);
+        } catch (FilaCheia e) {
+            System.out.println(e);
+        }
+
+        System.out.println(filaSequencial.remover());
+        System.out.println(filaSequencial.remover());
+        System.out.println(filaSequencial.remover());
+    }
+}
